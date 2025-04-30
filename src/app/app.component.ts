bebb245484d314bad3b191b39fe2983f6874e151
= <=>
2025-04-30 11:56:26 +0530

    feat: show/hide password with signal and [type] binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,22 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Counter App Example';
-  count = signal(0);
-
-  increment() {
-    console.log("Increment method called !");
-    this.count.update(value => value + 1);
+  isVisible = signal<boolean>(false);
+  inputType = computed(() => this.isVisible() ? 'text' : 'password'); // computed is read-only which has only single job to read the signals
+  toggleVisibility() {
+    // some logic to toggle
+    this.isVisible.update((value) => !value);
   }
-
-  decrement() {
-    console.log("Decrement method called !");
-    this.count.update(value => value && value - 1);
-  }
-
-  reset() {
-    console.log("Reset method called !");
-    this.count.set(0);
-  }
-
 }
