7ef0c4c0c46d3a5103a3a24e2b96c9bcc4d06fc1
= <=>
2025-05-07 14:34:52 +0530

    feat: configured templated driver form

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
@@ -12,5 +13,8 @@ export class AboutComponent {
   private route = inject(ActivatedRoute);
   ngOnInit() {
     console.log(Number(this.route.snapshot.paramMap.get('id')));
+  }
+  onSubmit(value: any) {
+    console.log('Form submitted', value);
   }
 }
