54007c05ade73f4208b243a7a2c986750ef5692c
= <=>
2025-05-07 15:02:47 +0530

    feat: added reactive forms with validators

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,21 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
   private route = inject(ActivatedRoute);
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  })
   ngOnInit() {
     console.log(Number(this.route.snapshot.paramMap.get('id')));
   }
-  onSubmit(value: any) {
-    console.log('Form submitted', value);
+
+  get email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  onSubmit() {
+    console.log('Form submitted', this.loginForm.value);
   }
 }
