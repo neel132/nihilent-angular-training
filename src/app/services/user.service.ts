2eb38b9463805ce16fd92ba1ee139fc4f5af5643
= <=>
2025-05-08 12:43:44 +0530

    feat: setup api configuration based upon the environment

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,20 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post, User } from '../models/user.model';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root' // Singleton service
 })
 
 export class UserService {
   private http = inject(HttpClient);
-  private readonly apiUrl = "https://jsonplaceholder.typicode.com/users";
-  private readonly apiUrlPosts = "https://jsonplaceholder.typicode.com/posts";
+  private baseUrl = environment.baseUrl;
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl);
+    return this.http.get<User[]>(`${this.baseUrl}users`);
   }
 
   createPost(post: Post): Observable<Post> {
-    return this.http.post<Post>(this.apiUrlPosts, post);
+    return this.http.post<Post>(`${this.baseUrl}posts`, post);
   }
 }
